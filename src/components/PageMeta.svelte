93f1b42ee0bc3506868aa9f42fa0a9ae5c9c5167
dreamscached <dreamscache.d@gmail.com>
2024-03-29 18:20:20 +0100

    feature: rework pagedescription in favor of two new components for basic metadata and ogp/twitter

---
+++ src/components/PageMeta.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    export let title: string = $page.data.meta?.title ?? $page.data.ogp?.title;
+    export let description: string = $page.data.meta?.description ?? $page.data.ogp?.description;
+    export let keywords: string[] = $page.data.meta?.keywords ?? [];
+</script>
+
+<svelte:head>
+    {#if title}
+        <title>{title} &bull; MAS Web Utils</title>
+    {:else}
+        <title>MAS Web Utils</title>
+    {/if}
+
+    {#if description}<meta name="description" content={description} />{/if}
+    {#if keywords?.length}<meta name="keywords" content={keywords.join(", ")} />{/if}
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-	// Import components
-	import PageDescription from "../components/PageDescription.svelte";
-
 	// Include CSS (+ Tailwind)
 	import "../app.css";
+
+	// Components
+	import PageMeta from "../components/PageMeta.svelte";
+	import OpenGraph from "../components/OpenGraph.svelte";
 
 	// Svelte transitions
 	import { fly } from "svelte/transition";
 
 	// Layout/page data
 	export let data: Record<string, unknown>;
-	import { page } from "$app/stores";
 </script>
 
-{#key $page.route}
-	<!-- Page title, description & OGP data (updates whenever route changes) -->
-	<PageDescription url={$page.url.toString()} title={$page.data.title} description={$page.data.description} />
-{/key}
+<!-- Include SEO metadata -->
+<PageMeta />
+<OpenGraph />
 
 {#key data.url}
 	<div in:fly={{ x: -200, duration: 300, delay: 300 }} out:fly={{ x: 200, duration: 300 }}>
