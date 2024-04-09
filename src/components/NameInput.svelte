dd58b6e12086897e19a37e9718ac6712961a0335
dreamscached <dreamscache.d@gmail.com>
2024-04-09 16:50:50 +0200

    feature: add name input component

---
+++ src/components/NameInput.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let placeholder: string = "Enter your name";
+    export let maxlength: number = 20;
+</script>
+
+<input on:input type="text" {placeholder} {maxlength} autocapitalize="words" class="text-2xl border-2 border-neutral-300 rounded-md p-3 italic">
